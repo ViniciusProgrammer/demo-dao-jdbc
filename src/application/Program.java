2900c158b6b7d5d1d1adc710b175d0bff70a047d
ViniciusProgrammer <viniciusimdufrn@gmail.com>
2026-09-12 13:49:34 -0300

    feta: criada a entidade Departamento

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Program {
+    public static void main(String[] args) {
+        Departamento departamento1 = new Departamento(1, "Books");
+        System.out.println(departamento1);
+    }
+}
