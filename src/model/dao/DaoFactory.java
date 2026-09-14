f57c534bd790fb33e64c8cd78418bc25ac3b83e4
ViniciusProgrammer <vinicios.david.083@ufrn.edu.br>
2026-09-14 13:35:07 -0300

    feat: criada o DaoFactory para devolver a implementação do objeto

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,4 +1,9 @@
 package model.dao;
 
+import model.dao.impl.VendedorDaoJDBC;
+
 public class DaoFactory {
+    public static VendedorDAO criarVendedorDao() {
+        return new VendedorDaoJDBC();
+    }
 }
