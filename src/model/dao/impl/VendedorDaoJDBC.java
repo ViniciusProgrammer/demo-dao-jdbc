06c260e5bd8f5ddda11ff70586fb4295386bcd15
ViniciusProgrammer <vinicios.david.083@ufrn.edu.br>
2026-09-14 13:34:02 -0300

    feat: criada a classe que implementa a interface VendedorDao

---
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.impl;
+
+import model.dao.VendedorDAO;
+import model.entities.Vendedor;
+
+import java.util.List;
+
+public class VendedorDaoJDBC implements VendedorDAO {
+    @Override
+    public void insert(Vendedor obj) {
+
+    }
+
+    @Override
+    public void update(Vendedor obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Vendedor findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Vendedor> findAll() {
+        return List.of();
+    }
+}
