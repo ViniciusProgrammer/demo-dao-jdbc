f17a2899d962461d036693db3f475110f4062bb8
ViniciusProgrammer <vinicios.david.083@ufrn.edu.br>
2026-09-14 13:35:38 -0300

    instanciando o DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDAO;
 import model.entities.Departamento;
 
 public class Program {
     public static void main(String[] args) {
         Departamento departamento1 = new Departamento(1, "Books");
         System.out.println(departamento1);
+
+        VendedorDAO vendedorDAO = DaoFactory.criarVendedorDao();
     }
 }
