c21d182ec53b2bb9ff564a5aa6bd2b0efb4ace98
ViniciusProgrammer <viniciusimdufrn@gmail.com>
2026-09-16 20:29:32 -0300

    fix: resolvido o erro do getConnection

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package model.dao;
 
+import exceptions.DB;
 import model.dao.impl.VendedorDaoJDBC;
 
 public class DaoFactory {
     public static VendedorDAO criarVendedorDao() {
-        return new VendedorDaoJDBC();
+        return new VendedorDaoJDBC(DB.getConnection());
     }
 }
